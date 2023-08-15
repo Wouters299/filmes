0855c0fe532ddcb18703dfd3031338e56fa9790d
Jordano Poletto <90645419+Jordano27@users.noreply.github.com>
2023-08-15 17:48:21 -0300

    adicao de rotas
    adicao de rotas users e filmes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\FilmesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
+});
+
+Route::prefix('/usuarios')->middleware("auth")->group(function () {
+
+    Route::get('add', [UsuariosController::class, 'add'])->name('usuarios.add');
+
+    Route::post('add', [UsuariosController::class, 'addSave'])->name('usuarios.addSave');
+
+    Route::get('edit', [UsuariosController::class, 'edit'])->name('usuarios.edit');
+
+    Route::post('editsave', [UsuariosController::class, 'editSave'])->name('usuarios.editSave');
+
+    Route::get('delete/{usuario}', [UsuariosController::class, 'delete'])->name('usuarios.delete');
+
+    Route::delete('delete/{usuario}', [UsuariosController::class, 'deleteForReal'])->name('usuarios.deleteForReal');
+
+});
+
+Route::prefix('/filmes')->middleware("auth")->group(function () {
+
+    Route::get('index', [FilmesController::class, 'index'])->name('filmes.index');
+
+    Route::get('add', [FilmesController::class, 'add'])->name('filmes.add');
+
+    Route::post('add', [FilmesController::class, 'addsave'])->name('filmes.addsave');
+
+    Route::get('edit/{produto}', [FilmesController::class, 'edit'])->name('filmes.edit');
+
+    Route::post('edit/{produto}', [FilmesController::class, 'editSave'])->name('filmes.editSave');
+
+    Route::get('delete/{produto}', [FilmesController::class, 'delete'])->name('filmes.delete');
+
+    Route::delete('delete/{produto}', [FilmesController::class, 'deleteForReal'])->name('filmes.deleteForReal');
+
 });
