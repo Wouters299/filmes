4e812fca91b4f54718524fab7b59ecfcf7c68ca5
Jordano Poletto <90645419+Jordano27@users.noreply.github.com>
2023-08-17 08:48:10 -0300

    inserindo o use do loginController

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@
 use App\Http\Controllers\AdminsController;
 use App\Http\Controllers\FilmesController;
 use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\LoginController;
 
 use Illuminate\Support\Facades\Route;
 
